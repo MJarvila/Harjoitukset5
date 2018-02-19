aa650cf4668e49fcb95eceaf891c396756596f28
MJarvila <35224611+MJarvila@users.noreply.github.com>
2018-02-19 20:05:52 +0200

    Add files via upload

---
+++ Harj5Teht2/Har5Teht2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Har5Teht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a date:");
+            string[] dateStrings = { Console.ReadLine() };
+            DateTime dateValue;
+            // Console.WriteLine("Attempting to parse strings using {0} culture.", CultureInfo.CurrentCulture.Name);
+
+            foreach (string dateString in dateStrings)
+            {
+                if (DateTime.TryParse(dateString, out dateValue))
+                    Console.WriteLine("True");
+                   // Console.WriteLine("  Converted '{0}' to {1} ({2}).", dateString, dateValue, dateValue.Kind);
+                else
+                    Console.WriteLine("Invalid date '{0}'.", dateString);
+            }
+        }
+    }
+}
