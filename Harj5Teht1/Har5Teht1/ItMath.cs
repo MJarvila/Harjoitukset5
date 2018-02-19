22bd3354fb769f63233207b95d7664dbbf7c8dc4
MJarvila <35224611+MJarvila@users.noreply.github.com>
2018-02-19 20:04:32 +0200

    Add files via upload

---
+++ Harj5Teht1/Har5Teht1/ItMath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Har5Teht1
+{
+    public class ItMath
+    {
+        private string input;
+        public string Input
+        {
+            get { return input; }
+            set { input = value; }
+        }
+        private static bool isNumeric;
+        public static bool IsNumeric(string Input)
+        {
+            double n;
+            bool isNumeric = double.TryParse(Input, out n);
+            Console.WriteLine(isNumeric);
+            return isNumeric;
+        }
+
+    }
+}
---
+++ Harj5Teht1/Har5Teht1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Har5Teht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Give a line; I'll check if it's a valid numeric.");
+                string input = Console.ReadLine();
+                double n;
+                bool isNumeric = double.TryParse(input, out n);
+                Console.WriteLine(isNumeric);
+            }
+
+        }
+    }
+}
